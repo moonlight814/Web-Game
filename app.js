59a44cac230d64211e4d26ff1a907f4201bc76f9
Natalia <natyargumedo@hotmail.com>
2022-02-28 21:16:57 -0800

    added functions to js that inputs users keys into first row

--- app.js
+++ app.js
@@ -21,8 +21,40 @@ function initialize () {
             let tile= document.createElement("span");  //span doesnt end with a new line
             tile.id = i.toString() + "-" + c.toString();
             tile.classList.add("boxes");
-            tile.innerText= "P"
+            tile.innerText= "";
             document.getElementById("container").appendChild(tile);
         }
     }
 }
+
+
+//Event listener for keyup because then only one letter will be inputted even if the key is held for a long time
+
+
+document.addEventListener("keyup", (e)=> { //e = key event
+    if (gameOver)return;
+    
+    
+    alert(e.key);
+    
+    //check to see which key was pressed by user
+    
+    if("KeyA" <= e.code && e.code <= "KeyZ"){
+        if(col<width){
+            let currentBox= document.getElementById(row.toString() + '-' + col.toString())
+            // console.log(currentBox)
+            if(currentBox.innerText == ``) {
+                currentBox.innerText= e.key.toUpperCase()
+                
+                col += 1;
+            }
+        }
+    }else if (e.code === 'Backspace'){
+        if ( 0 < col && col <= width){
+            col -=1;
+        }
+       
+        let currentBox= document.getElementById(row.toString() + '-' + col.toString());
+        currentBox.innerText = ``;
+    }
+})
--- input.js
+++ input.js
@@ -1,2 +1,24 @@
+
+
+
 //Event listener for keyup because then only one letter will be inputted even if the key is held for a long time
-document.addEventListener("keyup")
+
+
+// document.addEventListener("keyup", (e)=> { //e = key event
+//     if (gameOver)return;
+    
+    
+//     // alert(e.code);
+    
+//     //check to see if user pressed alphabet key
+    
+//     if("KeyA" <= e.code && e.code <= "KeyZ"){
+//         if(col<width){
+//             let currentBox= document.getElementById(row.toString + '-' + col.toString())
+//             if(currentBox.innerText == ""){
+//                 currentBox.innerText= e.code[3];
+//                 col += 1;
+//             }
+//         }
+//     }
+// })
